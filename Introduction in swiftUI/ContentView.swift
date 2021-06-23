c9b87ca7109a3bdffdbcb92a3bed6ea7cfadb9e4
uncoolbeans <ivcleandedmyroom@gmail.com>
2021-06-23 09:18:22 +0800

    Initial Commit

---
+++ Introduction in swiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Introduction in swiftUI
+//
+//  Created by MMS on 23/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Introduction in swiftUI/Introduction_in_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Introduction_in_swiftUIApp.swift
+//  Introduction in swiftUI
+//
+//  Created by MMS on 23/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Introduction_in_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
